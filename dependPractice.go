07a789af650cf54ee484b54e7d5b67da0e8d11dc
Esilahic <silahicemil11@gmail.com>
2023-07-13 09:26:41 -0600

    cleaning

--- dependPractice.go
+++ dependPractice.go
@@ -1,9 +1,21 @@
 package dependPractice
 
+import (
+	"github.com/Esilahic/dog"
+)
+
 func Practice() string {
 	return "Nice!"
 }
 
 func EvenMore() string {
 	return "Good Job!"
+}
+
+func OldPractice() string {
+	return dog.WhenGrownUp(Practice())
+}
+
+func OldMore() string {
+	return dog.WhenGrownUp(EvenMore())
 }
--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/Esilahic/dependPractice
 
 go 1.20
+
+require github.com/Esilahic/dog v0.0.0-20230713133550-29d133e59166
