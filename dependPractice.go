a3e9bb2870d49f3a47a5e6ca27f4fa2dbdd3f197
Esilahic <silahicemil11@gmail.com>
2023-07-13 06:56:07 -0600

    Package created

---
+++ dependPractice.go
@@ -0,0 +1,9 @@
+package dependPrac
+
+func Practice() string {
+	return "Nice!"
+}
+
+func EvenMore() string {
+	return "Good Job!"
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/Esilahic/dependPractice
+
+go 1.20
